16f17ece34da0f771a6e16c5ffe0071d8fbf6df1
razmiz <50333295+razmiz@users.noreply.github.com>
2020-03-26 23:13:02 +0200

    update animations

--- MemoryGame/MemoryGame/ViewController.swift
+++ MemoryGame/MemoryGame/ViewController.swift
@@ -27,6 +27,10 @@ class ViewController: UIViewController {
     var timer:Timer?
     var timeLeft = 60
     
+    var currentTag1: Int = -1
+    var currentTag2: Int = -1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         shuffleCards()
@@ -37,38 +41,48 @@ class ViewController: UIViewController {
     }
     
     @IBAction func cardButtonTapped(_ sender: UIButton) {
-        let emoji = emojiArray[sender.tag % 10]
+        let emoji = emojiArray[sender.tag]
         if sender.getTitle() != emoji {
             sender.setTitle(emoji)
+            
             
             switch taps {
             case 0:
                 cardsTuple.card1Tag = sender.tag
                 cardsTuple.card1Emoji = emoji
                 taps += 1
+                currentTag1 = sender.tag
             case 1:
+                view.isUserInteractionEnabled = false
                 cardsTuple.card2Tag = sender.tag
                 cardsTuple.card2Emoji = emoji
+                currentTag2 = sender.tag
                 
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
                     self.checkMatch()
                     self.cardsTuple = (0, " ", 0, " ")
                     self.checkEndOfGame()
+                    self.view.isUserInteractionEnabled = true
                 }
                 fallthrough
             default:
                 taps = 0
+                
             }
+        }else {
+            resetShape(button: sender)
         }
     }
     
     func checkMatch(){
         if !checkCardsEmoji() {
             clearCards()
+            animateMismatchCards(cardsToAnimate: [cardButtons[currentTag1],cardButtons[currentTag2]])
         } else {
             tagsArray[cardsTuple.card1Tag] = -1
             tagsArray[cardsTuple.card2Tag] = -1
             showToast(message: "YAY 😊!!!")
+            animateMatchCards()
             currentScore += 5
             setScore()
         }
@@ -82,7 +96,7 @@ class ViewController: UIViewController {
     func clearCards(){
         for button in cardButtons{
             if tagsArray.contains(button.tag){
-                button.setTitle(" ", for: .normal)
+                button.setTitle(" ")
             }
         }
         currentScore -= 1
@@ -90,18 +104,18 @@ class ViewController: UIViewController {
     }
     
     func shuffleCards(){
+        emojiArray += emojiArray
         emojiArray.shuffle()
     }
     
     func configureButton(){
         var number = 0
-        let shuffledArray = tagsArray.shuffled()
-        
         for button in cardButtons{
             button.layer.cornerRadius = 10
             button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
-            button.tag = shuffledArray[number]
+            button.tag = tagsArray[number]
             number += 1
+            resetShape(button: button)
         }
     }
     
@@ -110,7 +124,7 @@ class ViewController: UIViewController {
             sethighestScore()
         }
         if tagsArray.sorted().last == -1{
-            showAlert(title: "Well Done! 🙂", message: "play again?")
+            showAlert(title: "Well Done! 🙂", message: "play again?", exitCode: 0)
             timer!.invalidate()
             timer = nil
         }
@@ -120,20 +134,14 @@ class ViewController: UIViewController {
     func resetGame(){
         saveScore(scoreToSave: highestScore)
         tagsArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+        emojiArray = ["🐶", "🐸", "🐣", "🐱", "🐠", "🦞", "🐝", "🐳", "🦄", "🐓"]
         clearCards()
         currentScore = 0
-        viewDidLoad()
         timeLeft = 60
-    }
-    
-    
-    @IBAction func newGameAction(_ sender: UIButton) {
-        tagsArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-        clearCards()
-        currentScore = 0
+        taps = 0
+        animateMismatchCards(cardsToAnimate: cardButtons)
         viewDidLoad()
     }
-    
     
     func setScore(){
         if currentScore < 0 {
@@ -160,7 +168,7 @@ class ViewController: UIViewController {
         return preferences.integer(forKey: UserDefaultCodingKeys.currentScore.rawValue)
     }
     
-    func showAlert(title: String, message: String){
+    func showAlert(title: String, message: String, exitCode: Int){
         let alertConroller = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okButton = UIAlertAction(title: "OK", style: .default, handler: {
             UIAlertAction in
@@ -168,7 +176,12 @@ class ViewController: UIViewController {
         })
         let cancelButton = UIAlertAction(title: "Cancel", style: .destructive, handler: {
             UIAlertAction in
-            exit(0)
+            if exitCode == 0{
+                exit(0)
+            } else{
+                self.dismiss(animated: true, completion: nil)
+                self.continueGame()
+            }
         })
         alertConroller.addAction(okButton)
         alertConroller.addAction(cancelButton)
@@ -191,12 +204,52 @@ class ViewController: UIViewController {
             timerLabel.text = "Timer: 00:0\(timeLeft)"
         }
         if timeLeft <= 0 {
-            timer!.invalidate()
+            timer?.invalidate()
             timer = nil
-            showAlert(title: "Times Up!", message: "Try again?")
+            showAlert(title: "Times Up!", message: "Try again?", exitCode: 0)
         }
     }
     
+    func animateMatchCards (){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.cardButtons[self.currentTag1].transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            self.cardButtons[self.currentTag2].transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        }, completion: { _ in UIView.animate(withDuration: 0.3){
+            self.cardButtons[self.currentTag1].transform = CGAffineTransform.identity
+            self.cardButtons[self.currentTag2].transform = CGAffineTransform.identity
+            }
+        })
+    }
+    
+    func resetShape (button: UIButton){
+        UIView.animate(withDuration: 0.1, animations: {
+            button.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }, completion: { _ in UIView.animate(withDuration: 0.1){
+            button.transform = CGAffineTransform.identity
+            }
+        })
+    }
+    
+    
+    func animateMismatchCards(cardsToAnimate: [UIButton]){
+        cardsToAnimate.forEach {
+            UIView.transition(with: cardButtons[$0.tag],
+                              duration: 0.4,
+                              options: [.transitionFlipFromLeft ],
+                              animations: nil,
+                              completion: nil)
+        }
+    }
+    
+    @IBAction func resetGameButton(_ sender: UIButton) {
+        timer?.invalidate()
+        timer = nil
+        showAlert(title: "Reset Game", message: "Are You Sure?", exitCode: -1)
+    }
+    
+    func continueGame(){
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerFires), userInfo: nil, repeats: true)
+    }
 }
 
 
